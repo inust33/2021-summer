d5055fd3bfab076729d0557df514bcd61d759e0b
eugene663 <yujin663@gmail.com>
2021-08-20 13:07:14 +0900

    eugene663 week3

--- js/background.js
+++
@@ -1,7 +0,0 @@
-const bgColor=["YellowGreen", "LightBlue", "LightPink", "Plum"];
-const chosenColor = bgColor[Math.floor(Math.random() * bgColor.length)];
-
-const body = document.querySelector("body");
-
-console.log(chosenColor);
-body.bgColor = chosenColor;
