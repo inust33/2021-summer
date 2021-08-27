5942896476541287bf0b9d8e1e69854e0f7b5678
LeeSeohyun <mlsh1112@ajou.ac.kr>
2021-08-27 09:38:40 +0900

    Logout

--- 4주차/FE/mlsh1112/client/src/components/views/LandingPage/LandingPage.js
+++ 4주차/FE/mlsh1112/client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,20 @@
+import axios from 'axios';
 import React from 'react';
+import Feed from '../../../utils/Feed';
 
-const LandingPage = () => {
+const LandingPage = (props) => {
+    function onClickHandler (){
+        axios.get('/api/user/logout')
+        .then(res=>{
+            if(res.data.success) props.history.push('/login')
+            else alert ("Logout Fail")
+        })
+    }
     return (
         <div style={{display:'flex', justifyContent:'center',alignContent:'center'}}>
             LandingPage
+            <button onClick={onClickHandler}>Logout</button>
+            <Feed></Feed>
         </div>
     );
 }
